fe56563d77ff11560072cbd74fb0b31ad899216e
Umberto <Umbertogotti1979@gmail.com>
2019-01-23 08:26:54 +0100

    First commit.

---
+++ RabbitEndPoint1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+
+namespace RabbitEndPoint1
+{
+    using System;
+    using System.Text;
+    using RabbitMQ.Client;
+    using RabbitMQ.Client.Events;
+
+    class Receive
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory();
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += async (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                    await Task.Yield();
+                };
+                channel.BasicConsume(queue: "hello",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitEndPoint2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace RabbitEndPoint2
+{
+    using System;
+    using RabbitMQ.Client;
+    using System.Text;
+
+    class Send
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory();
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: "hello",
+                    basicProperties: null,
+                    body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
